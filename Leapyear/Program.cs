ce26a855a9e818a8be01b8c01fe3b72b44451a35
emman <emmanuelede206@gmail.com>
2024-02-29 16:04:14 +0100

    Add project files.

---
+++ Leapyear/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int currentYear = DateTime.Now.Year;
+
+        int leapYearCount = 0;
+
+        Console.WriteLine("Next 20 Leap Years:");
+
+        while (leapYearCount < 20)
+        {
+
+            if (IsLeapYear(currentYear))
+            {
+                Console.WriteLine(currentYear);
+
+                leapYearCount++;
+            }
+
+            currentYear++;
+        }
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0) && (year % 100 != 0);
+    }
+}
